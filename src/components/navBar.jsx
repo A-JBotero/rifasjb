6297ad463b02d7970e9f67a44c9ef077f0e4db33
unknown <juli57421@gmail.com>
2025-01-04 15:53:40 -0500

    solucion de errores mostrados en consola #2

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -16,6 +16,12 @@ const NavBar = ({ onAddCard, userRole, setUserRole }) => {
     navigate('/login'); // Navega a la página de login
   };
 
+  // Función para manejar el logout
+  const handleLogout = () => {
+    setUserRole(null); 
+    navigate('/login'); // Redirige a la página de login
+  };
+
   return (
     <header className="text-gray-600 body-font bg-primary">
       <div className="container mx-auto flex flex-wrap p-8 flex-col md:flex-row items-center">
@@ -23,10 +29,9 @@ const NavBar = ({ onAddCard, userRole, setUserRole }) => {
           href="/home"
           className="flex title-font font-medium items-center text-gray-900 mb-3 md:mb-0"
         >
-          <span className="ml-12 pt-2 text-2xl text-white">RIFAS BOCADILLO</span>
+          <span className="ml-12 pt-2 text-2xl text-white font-serif italic">RIFAS BOCADILLO</span>
         </a>
 
-        
         {userRole === 'admin' && (
           <button
             onClick={onAddCard}
@@ -36,13 +41,21 @@ const NavBar = ({ onAddCard, userRole, setUserRole }) => {
           </button>
         )}
 
-        
-        {isHomePage && !isLoginPage && (
+        {isHomePage && !isLoginPage && userRole !== 'admin' && (
           <button
-            onClick={handleAdminClick} 
+            onClick={handleAdminClick}
             className="inline-flex items-center text-white bg-blue-500 border-0 py-2 px-5 focus:outline-none hover:bg-blue-600 rounded ml-auto"
           >
             ADMINISTRAR
+          </button>
+        )}
+
+        {userRole === 'admin' && (
+          <button
+            onClick={handleLogout}
+            className="inline-flex items-center text-white bg-red-500 border-0 py-2 px-5 focus:outline-none hover:bg-red-600 rounded ml-auto"
+          >
+            LogOut
           </button>
         )}
       </div>

9ed9837d8ae95c003bf204aa6e4a546404ee2469
unknown <juli57421@gmail.com>
2025-04-18 19:13:08 -0500

    Ajuste de responsive en home 1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,9 +5,9 @@ import Footer from "../components/footer";
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-darkgray"> {/* Contenedor principal */}
+    <div className="flex flex-col min-h-screen bg-darkgray"> 
       <NavBar />
-      <main className="flex-grow"> {/* Contenedor de contenido flexible */}
+      <main className="flex-grow"> 
         <Blocks />
       </main>
       <Footer />
